c3f007c854fd81f38d98c1851ea7dfe7ba5d1ef2
ofiregev1 <ofirr@at-bay.com>
2024-05-15 12:14:57 +0300

    updating "main" file

--- run.py
+++ run.py
@@ -1,29 +1,18 @@
-import logging
-
-from csv_to_x_y_z import read_csv_sat_data, compute_receiver_pos
-
-logging.basicConfig(filename='receiver_positions.log', level=logging.INFO, format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+import os
+import sys
+from gnss_to_csv import parse_gnss_log
+from solution import calculate_locations, export_to_kml
 
 
-def main():
-    # File path to CSV data
-    file_path = 'gnss_log_2024_04_13_19_51_17_output.csv'
-
-    # Read satellite data from CSV file
-    sat_data = read_csv_sat_data(file_path)
-
-    # Compute receiver position for all GPS times
-    receiver_positions = compute_receiver_pos(sat_data)
-
-    # Print receiver positions
-    # for gps_time, pos in receiver_positions.items():
-    #     print(f"Receiver position at GPS time {gps_time}: {pos}")
+def main(input_path, output_path):
+    measurements = parse_gnss_log(input_path)
+    coordinates = calculate_locations(measurements)
+    kml_output_filepath = os.path.join(output_path, 'data', 'output.kml')
+    export_to_kml(coordinates, kml_output_filepath)
 
 
-#     # Write receiver positions to log file
-    for gps_time, pos in receiver_positions.items():
-        logging.info(f"Receiver position at GPS time {gps_time}: {pos}")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    input_filepath = sys.argv[1]
+    output_directory = sys.argv[2]
+    output_filename = sys.argv[3]
+    main(input_filepath,output_directory)
